78d9c40cfe62f26787d8266812ecc6ca85031f37
corasan <henrypl360@gmail.com>
2016-08-07 15:38:04 -0400

    added a logo to the form

--- FormView.js
+++ FormView.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
-import { View } from 'react-native';
+import { View, Image } from 'react-native';
 
 export default class FormView extends Component {
+    static get defaultProps() {
+        return { formLogo: require('./login-logo.png')};
+    }
+
     render() {
         return (
-            <View style={{paddingLeft: 16, paddingRight: 16}}>
+            <View style={{paddingLeft: 16, paddingRight: 16, marginTop: 50}}>
+                <Image source={this.props.formLogo} />
                 {this.props.children}
             </View>
         );
